54ea52f281c32f388fbb0701e308e5a242477c73
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2023-12-17 16:44:36 +0000

    redo for removed button

--- MultiServoTest/src/main.cpp
+++ MultiServoTest/src/main.cpp
@@ -10,14 +10,13 @@ const int potPin4 = 3, pinky = D4;
 const int potPin5 = 4, indexFinger = D5;
 const int potPin6 = 5, thumb = D6;
 
-#define DATA_PIN D0
+#define DATA_PIN D7
 #define NUM_LEDS 7
 
 CRGB leds[NUM_LEDS];
 
 int pos = 0;    
 int gHue= 0;    
-int buttonPin = D10;
 int buttonState = HIGH;   // HIGH when the button is not pressed, LOW when pressed
 int lastButtonState = HIGH;  // Previous state of the button
 unsigned long lastDebounceTime = 0;  // Last time the button state was changed
@@ -25,35 +24,15 @@ unsigned long debounceDelay = 90;  // Debounce time in milliseconds
 
 int positions[3] = {0, 90, 180};
 int positions2[3] = {180, 90, 0};
-int positionState = D10;
+int positionState = 0;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
-
   FastLED.addLeds<SK6812, DATA_PIN>(leds, NUM_LEDS); 
   FastLED.setBrightness(50);
 }
 
-void button() {
-  int reading = digitalRead(buttonPin);
 
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
-  }
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
-      if (reading == HIGH) {
-        positionState = (positionState+1)%3;
-      }
-      Serial.println(positionState);
-    }
-  }
-  lastButtonState = reading;
-}
 void loop() {
-  button();
-
   myservo.write(thumbBase, positions2[positionState]);
   myservo.write(thumb, positions2[positionState]);
   myservo.write(indexFinger, positions[positionState]);
@@ -62,7 +41,9 @@ void loop() {
   myservo.write(pinky, positions2[positionState]);
 
 
-    fill_rainbow( leds, NUM_LEDS, gHue, 25);
-    FastLED.show();
+  fill_rainbow( leds, NUM_LEDS, gHue, 25);
+  FastLED.show();
+
   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_MILLISECONDS( 3000 ) { positionState = (positionState+1)%3; } // slowly cycle the "base color" through the rainbow
 }
