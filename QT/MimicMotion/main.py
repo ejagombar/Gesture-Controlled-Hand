24054c77f870507b887d7869bae5cc03857c936c
Chip <ejagombar@gmail.com>
2023-12-28 20:18:55 +0000

    mediapipe example working

--- QT/MimicMotion/main.py
+++ QT/MimicMotion/main.py
@@ -1,10 +1,9 @@
 import sys
-from PySide6.QtCore import Qt, QThread, Signal, Slot, QObject
+import cv2
+import mediapipe as mp
+from PySide6.QtCore import Qt, QThread, Signal, Slot
 from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
-import mediapipe as mp
-
-import cv2
 
 
 class Thread(QThread):
@@ -12,16 +11,11 @@ class Thread(QThread):
 
     def __init__(self, parent=None):
         QThread.__init__(self, parent)
-        self.trained_file = None
         self.status = True
+        self.mp_hands = mp.solutions.hands.Hands()
+        self.mp_drawing = mp.solutions.drawing_utils
 
     def run(self):
-        base_options = mp.tasks.BaseOptions(model_asset_path="./face_landmarker.task")
-
-        options = mp.tasks.vision.FaceLandmarkerOptions(
-            base_options=base_options, running_mode=mp.tasks.vision.RunningMode.IMAGE
-        )
-
         cap = cv2.VideoCapture(0)
 
         while self.status:
@@ -29,12 +23,22 @@ class Thread(QThread):
             if not ret:
                 continue
 
+            # Convert the frame to RGB
             color_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Process the frame with MediaPipe
+            results = self.mp_hands.process(color_frame)
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    self.mp_drawing.draw_landmarks(
+                        color_frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS
+                    )
 
             h, w, ch = color_frame.shape
             img = QImage(
                 color_frame.data, w, h, ch * w, QImage.Format.Format_RGB888
-            ).scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
+            ).scaled(640 * 2, 480 * 2, Qt.AspectRatioMode.KeepAspectRatio)
 
             self.updateFrame.emit(img)
 
@@ -45,11 +49,11 @@ class Thread(QThread):
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Gesture Control")
-        self.setGeometry(0, 0, 640, 480)
+        self.setWindowTitle("Hand Landmarks Detection")
+        self.setGeometry(0, 0, 640 * 2, 480 * 2)
 
         self.label = QLabel(self)
-        self.label.setFixedSize(640, 480)
+        self.label.setFixedSize(640 * 2, 480 * 2)
 
         self.th = Thread(self)
         self.th.finished.connect(self.close)
@@ -66,8 +70,8 @@ class Window(QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QApplication()
+    app = QApplication([])
     w = Window()
     w.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
 
