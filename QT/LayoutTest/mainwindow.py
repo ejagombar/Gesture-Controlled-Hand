7c9932cc1ec2f56291b3a28a34addedbbdaaf14e
Chip <ejagombar@gmail.com>
2023-12-29 22:09:40 +0000

    customise themse and add tabs

--- QT/LayoutTest/mainwindow.py
+++ QT/LayoutTest/mainwindow.py
@@ -1,6 +1,4 @@
-# This Python file uses the following encoding: utf-8
 import sys
-import qdarkstyle
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
@@ -10,6 +8,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -18,11 +17,7 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
-
-    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
-    # app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyside6'))
 
     with open("OneLight.qss", "r") as f:
         _style = f.read()
