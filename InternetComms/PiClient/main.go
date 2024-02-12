4e9636c9abea1cbfaaaf58a0f71f789e7dacc759
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-02-12 22:22:02 +0000

    add unmarshalling data into struct client

--- InternetComms/PiClient/main.go
+++ InternetComms/PiClient/main.go
@@ -7,6 +7,19 @@ import (
 	"os"
 )
 
+type FingerData struct {
+	ThumbAngle    uint8
+	Thumb         uint8
+	Index         uint8
+	Middle        uint8
+	Ring          uint8
+	Pinky         uint8
+	LedColour     [3]uint8
+	LedBrightness uint8
+	ServoData     uint8
+	checkSum      uint16
+}
+
 func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
@@ -29,7 +42,7 @@ func main() {
 	defer conn.Close()
 
 	// Identify as a sender
-	_, err = conn.Write([]byte("sender"))
+	_, err = conn.Write([]byte("client1"))
 	if err != nil {
 		fmt.Println("Error identifying as a sender:", err)
 		return
@@ -52,6 +65,36 @@ func main() {
 	}
 }
 
+func unpackData(buffer []byte) (*FingerData, error) {
+	var receivedData FingerData
+	print("Buffer: ", buffer, "\n")
+	print(string(buffer), "\n")
+
+	if len(buffer) < 13 {
+		return nil, fmt.Errorf("Buffer too short")
+	}
+
+	receivedData.ThumbAngle = buffer[0]
+	receivedData.Thumb = buffer[1]
+	receivedData.Index = buffer[2]
+	receivedData.Middle = buffer[3]
+	receivedData.Ring = buffer[4]
+	receivedData.Pinky = buffer[5]
+	receivedData.LedColour[0] = buffer[6]
+	receivedData.LedColour[1] = buffer[7]
+	receivedData.LedColour[2] = buffer[8]
+	receivedData.LedBrightness = buffer[9]
+	receivedData.ServoData = buffer[10]
+	receivedData.checkSum = uint16(buffer[11])<<8 | uint16(buffer[12])
+
+	return &receivedData, nil
+}
+
+func checkSumMatch(fingerData FingerData) bool {
+	sum := int(fingerData.ThumbAngle) + int(fingerData.Thumb) + int(fingerData.Index) + int(fingerData.Middle) + int(fingerData.Ring) + int(fingerData.Pinky)
+	return sum == int(fingerData.checkSum)
+}
+
 func listen(conn *net.UDPConn) {
 	for {
 		buffer := make([]byte, 1024)
@@ -59,8 +102,29 @@ func listen(conn *net.UDPConn) {
 		if err != nil {
 			fmt.Println("Error reading from server:", err)
 			return
+
 		}
-		received := string(buffer[:n])
-		fmt.Println("Received:", received)
+		received, err := unpackData(buffer[:n])
+		if err != nil {
+			fmt.Println("Error unpacking data:", err)
+		}
+
+		if received != nil {
+
+			if !checkSumMatch(*received) {
+				print("---CHECKSUM MISMATCH---\n")
+			}
+
+			print("Thumb Angle: ", received.ThumbAngle, "\n")
+			print("Thumb: ", received.Thumb, "\n")
+			print("Index: ", received.Index, "\n")
+			print("Middle: ", received.Middle, "\n")
+			print("Ring: ", received.Ring, "\n")
+			print("Pinky: ", received.Pinky, "\n")
+			print("Led Colour: ", received.LedColour[0], received.LedColour[1], received.LedColour[2], "\n")
+			print("Led Brightness: ", received.LedBrightness, "\n")
+			print("Servo Data: ", received.ServoData, "\n")
+		}
+
 	}
 }
--- InternetComms/Server/main.go
+++ InternetComms/Server/main.go
@@ -1,70 +1,71 @@
 package main
 
 import (
-        "fmt"
-        "net"
-        "os"
-        "strings"
+	"fmt"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-        arguments := os.Args
-        if len(arguments) == 1 {
-                fmt.Println("Please provide a port number!")
-                return
-        }
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
 
-        PORT := ":" + arguments[1]
-        addr, err := net.ResolveUDPAddr("udp", PORT)
-        if err != nil {
-                fmt.Println("Error resolving address:", err)
-                return
-        }
+	PORT := ":" + arguments[1]
+	addr, err := net.ResolveUDPAddr("udp", PORT)
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
 
-        conn, err := net.ListenUDP("udp", addr)
-        if err != nil {
-                fmt.Println("Error listening:", err)
-                return
-        }
-        defer conn.Close()
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer conn.Close()
 
-        fmt.Println("Server listening on", addr)
+	fmt.Println("Server listening on", addr)
 
-        var client1, client2 *net.UDPAddr
+	var client1, client2 *net.UDPAddr
 
-        for {
-                buffer := make([]byte, 1024)
-                n, addr, err := conn.ReadFromUDP(buffer)
-                if err != nil {
-                        fmt.Println("Error reading from client:", err)
-                        continue
-                }
+	for {
+		buffer := make([]byte, 1024)
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Error reading from client:", err)
+			continue
+		}
 
-                clientAddr := addr.String()
-                message := string(buffer[:n])
+		clientAddr := addr.String()
+		message := string(buffer[:n])
 
-                if strings.ToLower(message) == "client1" {
-                        fmt.Println("Sender client connected:", clientAddr)
-                        client1 = addr
-                } else if strings.ToLower(message) == "client2" {
-                        fmt.Println("Receiver client connected:", clientAddr)
-                        client2 = addr
-                }
+		if strings.ToLower(message) == "client1" {
+			fmt.Println("Client1 connected:", clientAddr)
+			client1 = addr
+		} else if strings.ToLower(message) == "client2" {
+			fmt.Println("Client2 connected:", clientAddr)
+			client2 = addr
+		}
 
-                if client1 != nil && client2 != nil && addr.String() == client1.String() {
-                        _, err := conn.WriteToUDP(buffer[:n], client2)
-                        if err != nil {
-                                fmt.Println("Error forwarding data to client2:", err)
-                        }
-                        fmt.Printf("Forwarded message from client1 %s to client2 %s\n", clientAddr, client2.String())
-                }
+		if client1 != nil && client2 != nil && addr.String() == client1.String() {
+			_, err := conn.WriteToUDP(buffer[:n], client2)
+			if err != nil {
+				fmt.Println("Error forwarding data to client2:", err)
+			}
+			fmt.Printf("Forwarded message from client1 %s to client2 %s\n", clientAddr, client2.String())
+		}
 
-                if client1 != nil && client2 != nil && addr.String() == client2.String() {
-                        _, err := conn.WriteToUDP(buffer[:n], client1)
-                        if err != nil {
-                                fmt.Println("Error forwarding data to client1:", err)
-                        }
-                        fmt.Printf("Forwarded message from client2 %s to client1 %s\n", clientAddr, client1.String())
-                }
-        }
-};
+		if client1 != nil && client2 != nil && addr.String() == client2.String() {
+			_, err := conn.WriteToUDP(buffer[:n], client1)
+			if err != nil {
+				fmt.Println("Error forwarding data to client1:", err)
+			}
+			fmt.Printf("Forwarded message from client2 %s to client1 %s\n", clientAddr, client1.String())
+		}
+	}
+}
+
